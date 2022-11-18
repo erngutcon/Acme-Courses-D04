a5d6da1d26383750209b503fce9e17b550e56769
luicersan <luicersan@alum.us.es>
2022-11-18 14:40:59 +0100

    Task-003: fix validation

--- src/main/java/acme/features/any/blink/BlinkCreateService.java
+++ src/main/java/acme/features/any/blink/BlinkCreateService.java
@@ -75,6 +75,10 @@ public class BlinkCreateService implements AbstractCreateService<Any, Blink> {
 		assert entity != null;
 		assert errors != null;
 
+		boolean confirmation;
+
+		confirmation = request.getModel().getBoolean("confirmation");
+		errors.state(request, confirmation, "confirmation", "any.blink.form.label.confirmation");
 	}
 
 	@Override
